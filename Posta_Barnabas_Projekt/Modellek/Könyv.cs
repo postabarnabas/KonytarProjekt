5581795b8f070f72f834fdc0d1827596501b86d6
Posta barnabásgit config --global user.name Posta <postabarnabas1017@gmail.com>
2025-05-20 13:09:19 +0200

    Model mappa és a Könyv.cs fájl létrehozása

---
+++ Posta_Barnabas_Projekt/Modellek/Könyv.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Posta_Barnabas_Projekt.Modellek
+{
+    public class Könyv
+    {
+        [Key]
+        public int LeltáriSzám { get; set; }
+
+        [Required]
+        [RegularExpression(@"\S+", ErrorMessage ="A címnek tilos üresnek vagy csak egy szóköznek lennie.")]
+        public string Cím { get; set; }
+
+        [Required]
+        [RegularExpression(@"\S+", ErrorMessage = "A szerző nevének tilos üresnek vagy csak egy szóköznek lennie.")]
+        public string Szerző {  get; set; }
+
+        [Required]
+        [RegularExpression(@"\S+", ErrorMessage = "A kiadó nevének tilos üresnek vagy csak egy szóköznek lennie.")]
+        public string Kiadó {  get; set; }
+
+        [Range(0,int.MaxValue, ErrorMessage ="A kiadás éve nem lehet negatív")]
+        public int KiadásÉve {  get; set; }
+
+    }
+}
