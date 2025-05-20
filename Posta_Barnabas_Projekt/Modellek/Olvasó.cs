cf71c7edb7298f4a0842edb2b3f5374cad0a24a2
Posta barnabásgit config --global user.name Posta <postabarnabas1017@gmail.com>
2025-05-20 13:16:29 +0200

    Az Olvasó.cs osztály megírása

---
+++ Posta_Barnabas_Projekt/Modellek/Olvasó.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Posta_Barnabas_Projekt.Modellek
+{
+    public class Olvasó
+    {
+        [Key]
+        public int OlvasóSzám {  get; set; }
+
+        [Required]
+        [RegularExpression(@"\S+", ErrorMessage = "A névnek tilos üresnek vagy csak egy szóköznek lennie.")]
+        public string Név {  get; set; }
+
+        [Required]
+        [RegularExpression(@"\S+", ErrorMessage = "A lakcímnek tilos üresnek vagy csak egy szóköznek lennie.")]
+        public string Lakcím { get; set; }
+
+        [DataType(DataType.Date)]
+        [Range(typeof(DateTime),"1900-01-01","2025. 05. 15.",ErrorMessage ="A születési dátum nem lehet 1900 előtt.")]
+        public DateTime BirthDate { get; set; }
+    }
+}
