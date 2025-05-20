388529dc8390c4d7b449deb09562552e73b84f11
Posta barnabásgit config --global user.name Posta <postabarnabas1017@gmail.com>
2025-05-20 15:11:18 +0200

    A prgoram.cs-ben a service osztályok regisztrálása

--- Posta_Barnabas_Projekt/Program.cs
+++ Posta_Barnabas_Projekt/Program.cs
@@ -1,10 +1,15 @@
 using Posta_Barnabas_Projekt.Data;
 using Microsoft.EntityFrameworkCore;
+using Posta_Barnabas_Projekt.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<KönyvtárAdatbázis>(opciók => 
 opciók.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IKönyvService, KönyvService>();
+builder.Services.AddScoped<IOlvasóService, OlvasóService>();
+builder.Services.AddScoped<IKölcsönzésService, KölcsönzésService>();
 
 // Add services to the container.
 
