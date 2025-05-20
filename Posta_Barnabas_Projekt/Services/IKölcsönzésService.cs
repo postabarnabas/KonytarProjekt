6b318a6ea261b1ecc1520aef0530d8c4c4025360
Posta barnabásgit config --global user.name Posta <postabarnabas1017@gmail.com>
2025-05-20 14:59:45 +0200

    IKölcsönzésService implementálása

---
+++ Posta_Barnabas_Projekt/Services/IKölcsönzésService.cs
@@ -0,0 +1,8 @@
+﻿using Posta_Barnabas_Projekt.Modellek;
+
+public interface IKölcsönzésService
+{
+    Task<List<Kölcsönzés>> ListázásAsync();
+    Task<Kölcsönzés> LétrehozásAsync(Kölcsönzés kolcsonzes);
+    Task<List<Kölcsönzés>> LekérésOlvasóSzerintAsync(int olvasóId);
+}
