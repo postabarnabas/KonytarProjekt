8e183fcba525835d2449f307676e0ef306fabd38
Posta barnabásgit config --global user.name Posta <postabarnabas1017@gmail.com>
2025-05-20 14:08:32 +0200

    Data mappán belül a KönyvtárAdatbázis.cs megírása

---
+++ Posta_Barnabas_Projekt/Data/KönyvtárAdatbázis.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Posta_Barnabas_Projekt.Modellek;
+namespace Posta_Barnabas_Projekt.Data
+{
+    public class KönyvtárAdatbázis : DbContext
+    {
+        public KönyvtárAdatbázis(DbContextOptions<KönyvtárAdatbázis> opciók) : base(opciók) { }
+        
+        public DbSet<Könyv> Könyvek {  get; set; }
+        public DbSet<Olvasó> Olvasók { get; set; }
+        public DbSet<Kölcsönzés> Kölcsönzések { get; set; }
+    }
+}
