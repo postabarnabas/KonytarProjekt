c83bd07862cce3a4c87f45245f18528346361204
Posta barnabásgit config --global user.name Posta <postabarnabas1017@gmail.com>
2025-05-20 15:04:34 +0200

    KölcsönzésService osztály implementálása

---
+++ Posta_Barnabas_Projekt/Services/KölcsönzésService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Posta_Barnabas_Projekt.Data;
+using Posta_Barnabas_Projekt.Modellek;
+
+public class KölcsönzésService : IKölcsönzésService
+{
+    private readonly KönyvtárAdatbázis _context;
+
+    public KölcsönzésService(KönyvtárAdatbázis context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Kölcsönzés>> ListázásAsync()
+    {
+        return await _context.Kölcsönzések
+            .Include(k => k.LeltáriSzám)
+            .Include(k => k.OlvasóSzám)
+            .ToListAsync();
+    }
+
+    public async Task<Kölcsönzés> LétrehozásAsync(Kölcsönzés kolcsonzes)
+    {
+        _context.Kölcsönzések.Add(kolcsonzes);
+        await _context.SaveChangesAsync();
+        return kolcsonzes;
+    }
+
+    public async Task<List<Kölcsönzés>> LekérésOlvasóSzerintAsync(int olvasóId)
+    {
+        return await _context.Kölcsönzések
+            .Where(k => k.OlvasóSzám == olvasóId)
+            .Include(k => k.LeltáriSzám)
+            .ToListAsync();
+    }
+}
